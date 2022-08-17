27a497e9f043234df031f2678316a04d8ca908c3
javiercmelendrez <javier.c.melendrez@gmail.com>
2022-08-16 22:00:36 -0400

    finished work

--- designs.js
+++ designs.js
@@ -3,8 +3,29 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+const form = document.getElementById("sizePicker");
+form.addEventListener("submit", handleSubmit);
 
-// Your code goes here!
+function handleSubmit(e) {
+  e.preventDefault();
+  const height = e.target.elements.inputHeight.value;
+  const width = e.target.elements.inputWidth.value;
+  makeGrid(height, width);
+}
 
+function makeGrid(height, width) {
+  const table = document.getElementById("pixelCanvas");
+  table.innerHTML = "";
+  for (let i = 0; i < height; i++) {
+    let row = pixelCanvas.insertRow(i);
+
+    for (let j = 0; j < width; j++) {
+      let cell = row.insertCell(j);
+      cell.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        let color = document.getElementById("colorPicker");
+        cell.style.backgroundColor = color.value;
+      });
+    }
+  }
 }
